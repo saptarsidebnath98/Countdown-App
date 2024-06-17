814b15d5cef0327a53b51900e2c9b2101aff00d5
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-17 17:45:56 +0530

    useRef is used for handling the separate instances of Componenets in TimerChallenge.jsx

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,37 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function TimerChallenge({title, targetTime}){
+    const [timerStarted, setTimerStared] = useState(false);
+    const [timerExpired, SetTimerExpired] = useState(false);
+
+    let timer = useRef();
+
+    function handleStart(){
+        timer.current = setTimeout(()=> {
+            SetTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStared(true);
+    }
+
+    function handleStop(){
+        clearTimeout(timer.current);
+    }
     return (
         <section className="challenge">
            <h2>{title}</h2> 
+           {timerExpired && <p>You Lost!</p>}
            <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
            </p>
            <p>
-               <button>
-                Start Challenge
+               <button onClick={timerStarted? handleStop : handleStart}>
+                {timerStarted ? 'Stop' : 'Start'} Challenge
                </button>
            </p>
-           <p className="">
-                Time is running/ Time stopped
+           <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : ' Timer inactive'}
            </p>
         </section>
     )
