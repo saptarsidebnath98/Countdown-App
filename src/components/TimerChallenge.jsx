376305702c498143e59e01df5ed864c1a167a10c
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-17 20:21:29 +0530

    useImperativeHandle used to the ReasultModal.jsx that helped to useRef situational handling

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -13,7 +13,7 @@ export default function TimerChallenge({title, targetTime}){
     function handleStart(){
         timer.current = setTimeout(()=> {
             SetTimerExpired(true);
-            dialog.current.showModal();
+            dialog.current.open();
         }, targetTime * 1000);
 
         setTimerStared(true);
